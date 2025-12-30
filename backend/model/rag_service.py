fe02f86ba87a8d1048c7ffa2c5d2459ccdc25b4c
Anwesha Chakraborty <acanne.297@gmail.com>
2025-12-30 23:44:51 +0530

    Backend Working

--- backend/model/rag_service.py
+++ backend/model/rag_service.py
@@ -78,7 +78,7 @@ Summary:
 Details:
 Uncertainty:
 """
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.5-flash")
     response = model.generate_content(prompt)
 
     text = response.text.strip()
